4d70af7c2308333bfb5c948193680d2fdd9373ab
Mikko Mattila <mikkomattila@Mikkos-MacBook-Pro.local>
2016-04-04 10:28:54 +0300

    Change to api.js routing

--- src/api.js
+++ src/api.js
@@ -30,39 +30,39 @@ module.exports = (config, app) => {
     let responseFromCore = await core.getQuestionnaire(uuid);
     ctx.body = responseFromCore;
   }
-  
+
   async function deployQuestionnaire(ctx,uuid){
     let responseFromCore = await core.deployQuestionnaire(uuid);
     ctx.body = responseFromCore;
   }
-  
+
   async function closeQuestionnaire(ctx,uuid){
     let responseFromCore = await core.closeQuestionnaire(uuid);
     ctx.body = responseFromCore;
   }
-  
+
   async function updateQuestionnaire(ctx,uuid){
   	let payload = ctx.request.body;
     let responseFromCore = await core.updateQuestionnaire(uuid,payload);
     ctx.body = responseFromCore;
   }
-  
+
   async function getQuestions(ctx,path){
   	let lang = this.query.lang;
     let responseFromCore = await core.getQuestions(path,lang);
     ctx.body = responseFromCore;
   }
-  	 
+
 
   // ROUTES
 
   app.use(bodyParser());
-  app.use(route.get('/questionnaires', getQuestionnaires));
-  app.use(route.put('/questionnaire', putQuestionnaire));
-  app.use(route.delete('/questionnaire/:uuid', deleteQuestionnaire));
-  app.use(route.get('/questionnaire/:uuid', getQuestionnaire));
-  app.use(route.post('/questionnaire/:uuid/deploy', deployQuestionnaire));
-  app.use(route.post('/questionnaire/:uuid/close', closeQuestionnaire));
-  app.use(route.put('/questionnaire/:uuid', updateQuestionnaire));
-  app.use(route.get('/questions/:path',getQuestions));
+  app.use(route.get('/v1/questionnaires', getQuestionnaires));
+  app.use(route.put('/v1/questionnaires', putQuestionnaire));
+  app.use(route.delete('/v1/questionnaires/:uuid', deleteQuestionnaire));
+  app.use(route.get('/v1/questionnaires/:uuid', getQuestionnaire));
+  app.use(route.post('/v1/questionnaires/:uuid/deploy', deployQuestionnaire));
+  app.use(route.post('/v1/questionnaires/:uuid/close', closeQuestionnaire));
+  app.use(route.put('/v1/questionnaires/:uuid', updateQuestionnaire));
+  app.use(route.get('/v1/questions/:path',getQuestions));
 }
